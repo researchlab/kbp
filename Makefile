ecd8fb8f35b3368bdfa31f1e059542220203b700
lihong <lihong@jiedaibao.com>
2018-01-04 11:00:45 +0800

    first commit

---
+++ Makefile
@@ -0,0 +1,18 @@
+RELEASE=0.0.1
+COMMIT=$(shell git rev-parse --short HEAD)
+BUILD_TIME=$(shell data -u '+%Y-%m-%d_%H:%M:%S')
+PROJECT=advent
+
+APP=advent
+PORT=8000
+clean:
+	rm -f ${APP}
+build: clean
+	go build \ 
+	  -ldflags "-s -w -X ${PROJECT}/version.Release=${RELEASE} \
+			-X ${PROJECT}/version.Commit=${COMMIT} -X ${PROJECT}/version.BuildTime=${BUILD_TIME}" \
+			-o ${APP}
+run: build 
+	PORT=${PORT} ./${APP}
+test:
+	go test -v -race ./...
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/researchlab/advent-2017/handlers"
+	"github.com/researchlab/advent-2017/version"
+)
+
+func main() {
+	log.Printf("Starting the service...\ncommit: %s, build time: %s, release: %s",
+		version.Commit, version.BuildTime, version.Release)
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		log.Fatal("Port is not set.")
+	}
+	router := handlers.Router()
+	log.Print("The service is ready to listen and serve.")
+	log.Fatal(http.ListenAndServe(":"+port, router))
+}
